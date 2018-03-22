bd62f0835e9c371365e8e29180930731eee17eed
kpatel <kalepshjpatel@gmail.com>
2018-03-21 21:53:53 -0500

    exercise 11 completed

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weight?", end=' ')
+weight = input()
+print("howz is your day?", end=' ')
+mood = input()
+
+print(f"So, youre {age} old, {height} tall and {weight} heavy")
+print(f"I'm doing {mood}")
