4d12929dd6d2a39baf8bcf4bc50ded9f2f2dada5
kpatel <kalepshjpatel@gmail.com>
2018-03-21 21:45:16 -0500

    exercise 10 completed

---
+++ ex10.py
@@ -0,0 +1,18 @@
+tabby_cat ="I'm tabbed in."
+persiona_cat = "I'm split\non a line"
+backslah_cat = "i'm \\a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+new_cat = "\\ \' \tkalpesh"
+
+print(tabby_cat)
+print(persiona_cat)
+print(backslah_cat)
+print(fat_cat)
+print(new_cat)
