7a86890b05cf31dd43f21312cc0a208985945faf
kpatel <kalepshjpatel@gmail.com>
2018-03-24 17:49:06 -0500

    test

---
+++ ex12.py
@@ -0,0 +1,8 @@
+age = input("how old are you? ")
+height = input ("how tall are you? ")
+weight = input ("how much do you weight? ")
+
+
+print(f"so you are {age} old, {height} tall and {weight} heavy")
+
+#print("how old are you", input())
