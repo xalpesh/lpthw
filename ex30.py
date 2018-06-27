31768f3f87807373494de1370e419fa2f237aa95
kpatel <kalepshjpatel@gmail.com>
2018-06-26 20:35:36 -0500

    git commit

--- ex30.py
+++ ex30.py
@@ -5,7 +5,7 @@ trucks = 15
 
 if cars > people:
     print("We should take the cars.")
-elif cars < people:
+elseif cars < people:
     print("We should not take the cars.")
 else:
     print("We can't decide.")
