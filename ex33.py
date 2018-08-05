1693103d9fa6977572f37120e7c2cd8c5d55d1f4
kpatel <kalepshjpatel@gmail.com>
2018-08-05 18:27:55 -0500

    final save

---
+++ ex33.py
@@ -0,0 +1,20 @@
+i=10
+j=9
+
+mywhileloop(i,j)
+
+def mywhileloop(i,j):
+numbers = []
+
+while i<j:
+    print(f"at the top of the i is {i}")
+    numbers.append(i)
+
+    i = i+1
+    print("numbers now:", numbers)
+    print(f"At the bottom i is{i}")
+
+print("the numbers:")
+
+for num in numbers:
+    print(num)
